f7495885650add9b3d45af4067b46553418a05db
0WD0 <1105848296@qq.com>
2023-09-16 00:14:52 +0800

    9.16
    vector<string>

--- apps/RecitingAssistant.cpp
+++ apps/RecitingAssistant.cpp
@@ -1,18 +1,24 @@
 #include<bits/stdc++.h>
 using namespace std;
-const int N=100005;
-char s[N];
-map<char,int>en,pun;
-map<char,vector<int>>kanji;
-char ed[8]={'”','。','？','！','）','；','：'};
-struct wchar{
-	char s[3];
-}ws[N];
+const int N=10004;
+char cc[N];
+vector<string>s;
+template <typename T>
+vector<T> &operator +(vector<T> &v1,vector<T> &v2){
+	v1.insert(v1.end(),v2.begin(),v2.end());	
+	return v1;
+}
+int len;
+map<string,int>en,pun;
+map<string,vector<int>>kanji;
+const int ne=7,np=8;
+string ed[ne]={"”","。","？","！","）","；","："};
+string punctuation[np]={"《","》","（","“","，","、","‘","’"};
 struct NODE{
-	string s;
+	vector<string>s;
 	int L,R,fa;
 }nd[N];
-vector<int,int>g[N];
+vector<int>g[N];
 int t;//cur
 int lv[5];
 #define p lv[1]
@@ -25,35 +31,46 @@ void mlv(int i){
 	nd[t].L=lv[i],nd[lv[i]].R=t;nd[t].fa=lv[i];
 	lv[i]=t;
 }
-void test(){
-	for(int x:g[0]){
-		cout<<nd[x].s<<endl;
+void clean(){
+	int a=0,j=0;
+	for(;cc[j];j++,a++){
+		while(cc[j]&&(cc[j]==' '||cc[j]<='z'&&cc[j]>='a'))j++;
+		cc[a]=cc[j];
 	}
+	for(;a<=j;a++)cc[a]=0;
 }
-void get(){
+void tran(){
+	len=strlen(cc)/3;s.clear();
+	for(int i=0;i<len;i++)s.emplace_back((string)""+cc[i*3]+cc[i*3+1]+cc[i*3+2]);
+}
+void print(vector<string>&v){
+	for(auto x:v)cout<<x;
+	cout<<endl;
+}
+void test(){
+	for(int x:g[5]) print(nd[x].s);
 }
 int main(){
-	for(int i=0;i<8;i++)en[ed[i]]=1;
-	pun=en; pun['“']=pun['（']=pun['，']=1;
+	for(int i=0;i<ne;i++)en[ed[i]]=pun[ed[i]]=1;
+	for(int i=0;i<np;i++)pun[punctuation[i]]=1;
 	int n=1;
 	while(n--){
-		while(gets(s),s[0]=='\0');
+		for(;gets(cc),!cc[0];cc[0]=0);
+		clean();tran();
 		mlv(1); nd[t].s=s;
-		while(gets(s),s[0]){
-			int a=0,j=0;
-			for(;s[j];j++,a++){
-				while(s[j]&&(s[j]==' '||s[j]<='z'&&s[j]>='a'))j++;
-				s[a]=s[j];
-			}
-			for(;a<=j;a++)s[a]=0;
-			mlv(2); nd[p].s+=s; nd[d].s=s;
-			for(int i=0;s[i];i++){
-				if(i==0||!en.count(s[i])&&en.count(s[i-1]))mlv(3);
-				if(i==0||s[i-1]=='，')mlv(4);
-				nd[xj].s+=s[i];
-				nd[ju].s+=s[i];
+		// print(s);	
+		for(;gets(cc),cc[0];cc[0]=0){
+			clean();tran();
+			mlv(2); nd[p].s=nd[p].s+s; nd[d].s=s;
+			// print(s);	
+			for(int i=0;i<len;i++){
+				if(i==0||!en.count(s[i])&&en.count(s[i-1]))mlv(3),mlv(4);
+				else if(i==0||s[i-1]=="，")mlv(4);
+				nd[xj].s.emplace_back(s[i]);
+				nd[ju].s.emplace_back(s[i]);
 				if(pun.count(s[i]))kanji[s[i]].push_back(xj);
 			}
 		}
 	}
+	test();
 }
---
+++ tmp.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+char s[100];
+int main(){
+	gets(s);cout<<s[0];
+	cout<<(!s[0]);
+	gets(s);cout<<s[0];
+	cout<<(!s[0]);
+	gets(s);cout<<s[0];
+	cout<<(!s[0]);
+	gets(s);cout<<s[0];cout<<(!s[0]);
+}
