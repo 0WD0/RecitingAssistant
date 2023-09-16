0c7a771b83f5e21acea925801a667cbd149569ff
_WD_ <68629279+0WD0@users.noreply.github.com>
2023-09-16 23:14:16 +0800

    Delete tmp.cpp

--- tmp.cpp
+++
@@ -1,8 +0,0 @@
-#include<bits/stdc++.h>
-using namespace std;
-string s="⓪①②③④⑤⑥⑦⑧⑨⑩⑪⑫⑬⑭⑮⑯⑰⑱⑲⑳㉑㉒㉓㉔㉕㉖㉗㉘㉙㉚㉛㉜㉝㉞㉟㊱㊲㊳㊴㊵㊶㊷㊸㊹㊺㊻㊼㊽㊾㊿";
-int main(){
-	for(int i=0;i<153;cout<<endl,i++)
-	for(int j=0;j<=i;j++)
-	cout<<s[j];
-}
