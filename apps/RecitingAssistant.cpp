7468f4527e88c79a9f27384a78a3b1cc3f6835fb
0WD0 <1105848296@qq.com>
2023-09-17 14:18:42 +0800

    delay 优化

--- apps/RecitingAssistant.cpp
+++ apps/RecitingAssistant.cpp
@@ -17,14 +17,16 @@ vector<T> &operator +(vector<T> &v1,vector<T> &v2){
 	return v1;
 }
 int len;
-map<string,int>en,pun,cir;
-map<string,vector<int>>kanji;
-const int ne=7,np=9;
-string ed[ne]={"”","。","？","！","）","；","："};
-string punctuation[np]={"《","》","（","“","，","、","‘","’","·"};
-string ord[55]={"⓪","①","②","③","④","⑤","⑥","⑦","⑧","⑨","⑩","⑪","⑫","⑬","⑭","⑮","⑯","⑰","⑱","⑲","⑳","㉑","㉒","㉓","㉔","㉕","㉖","㉗","㉘","㉙","㉚","㉛","㉜","㉝","㉞","㉟","㊱","㊲","㊳","㊴","㊵","㊶","㊷","㊸","㊹","㊺","㊻","㊼","㊽","㊾","㊿"};
+map<string,int>en,mi;
+//map<string,int>cir;
+// map<string,vector<int>>kanji;
+const int ne=6,np=2;
+string md[np]={"，",":"};
+string ed[ne]={"。","？","！","）","；","："};
+// string punctuation[np]={"《","》","（","“","，","、","‘","’","·"};
+// string ord[55]={"⓪","①","②","③","④","⑤","⑥","⑦","⑧","⑨","⑩","⑪","⑫","⑬","⑭","⑮","⑯","⑰","⑱","⑲","⑳","㉑","㉒","㉓","㉔","㉕","㉖","㉗","㉘","㉙","㉚","㉛","㉜","㉝","㉞","㉟","㊱","㊲","㊳","㊴","㊵","㊶","㊷","㊸","㊹","㊺","㊻","㊼","㊽","㊾","㊿"};
 struct NODE{
-	vector<string>s;
+	vector<string>s,title;
 	int L,R,fa,c,od;
 }nd[N];
 vector<int>g[N];
@@ -37,6 +39,7 @@ int t,lv[6];
 void mlv(int i){
 	++t;nd[t].od=g[lv[i-1]].size();
 	g[lv[i-1]].push_back(t);
+	if(i>1)nd[t].title=nd[lv[i-1]].title;
 	nd[t].L=lv[i],nd[lv[i]].R=t;nd[t].fa=lv[i-1];
 	lv[i]=t;nd[t].c=i;
 }
@@ -66,9 +69,10 @@ void print(vector<string>&v){
 	cout<<endl;
 }
 void init(){
-	for(int i=0;i<ne;i++)en[ed[i]]=pun[ed[i]]=1;
-	for(int i=0;i<np;i++)pun[punctuation[i]]=1;
-	for(int i=0;i<=50;i++)cir[ord[i]]=1;
+	for(int i=0;i<np;i++)mi[md[i]]=1;
+	for(int i=0;i<ne;i++)en[ed[i]]=1;
+	// for(int i=0;i<np;i++)pun[punctuation[i]]=1;
+	// for(int i=0;i<=50;i++)cir[ord[i]]=1;
 }
 void processing_text(){
 	freopen("../IO/text.txt","r",stdin);
@@ -77,17 +81,22 @@ void processing_text(){
 	while(n--){
 		for(;gets(cc),!cc[0];cc[0]=0);
 		clean();tran();
-		mlv(1); nd[t].s=s;
+		mlv(1); nd[t].title=s;
 		for(;gets(cc),cc[0];cc[0]=0){
 			clean();tran();
-			mlv(2); nd[p].s.emplace_back("\n");nd[p].s=nd[p].s+s; nd[d].s=s;
-			for(int i=0;i<len;i++){
-				if(i==0||!en.count(s[i])&&en.count(s[i-1]))mlv(3),mlv(4);
-				else if(i==0||s[i-1]=="，")mlv(4);
+			mlv(2); nd[p].s=nd[p].s+s; nd[p].s.emplace_back("\n"); nd[d].s=s;
+			for(int la1=1,la2=1,i=0;i<len;i++){
+				if(la1&&!en.count(s[i])&&s[i]!="”")mlv(3),mlv(4),la2=la1=0;
+				else if(la2&&!en.count(s[i]))mlv(4),la2=0;
+				// if(i==0||pun.count(s[i-1])&&judge(i))mlv(3),mlv(4);
+				// else if(i==0||s[i-1]=="，")mlv(4);
+				
 				nd[xj].s.emplace_back(s[i]);
 				nd[ju].s.emplace_back(s[i]);
 				mlv(5);nd[dz].s.emplace_back(s[i]);
-				if(pun.count(s[i]))kanji[s[i]].push_back(xj);
+				// if(pun.count(s[i]))kanji[s[i]].push_back(xj);
+				if(en.count(s[i]))la1=1;
+				if(mi.count(s[i]))la2=1;
 			}
 		}
 	}
@@ -103,14 +112,17 @@ void check(char c){
 char keys[4]={'H','J','K','L'};
 int po,ind;
 void show(){
+	cout<<'|';for(auto x:nd[g[po][ind]].title)cout<<x;cout<<"|\n\n";
 	for(auto x:nd[g[po][ind]].s)cout<<x;
 }
 void display(){
-	if(down[7]&&nd[po].c!=0){//H
+	if(down[7]){//H
+		if(nd[po].c==0)return;
 		ind=nd[po].od;
 		po=nd[po].fa;
 	}
-	if(down[11]&&nd[po].c!=4){//L
+	if(down[11]){//L
+		if(nd[po].c==4)return;
 		po=g[po][ind];
 		ind=0;
 	}
@@ -127,13 +139,18 @@ void display(){
 }
 void keyboard_monitor(){
 	po=0;ind=0;
-    system("cls");
+	system("cls");
 	printf("ROOT");
 	while(1){
-	    pre=down;
+		pre=down;
 		for(auto c:keys)check(c);
-	    if(down.to_ulong()>pre.to_ulong())display();
-		Sleep(20);
+    	if(down.to_ulong()>pre.to_ulong()){
+			for(int delay=200;down.to_ullong()!=0;delay>=60&&(delay-=10)){
+				display();
+				Sleep(delay);
+				for(auto c:keys)check(c);
+			}
+		}
     }
 }
 
